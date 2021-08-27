5bd9d760caf9d7cbea9064a5582fa1845121abea
Jacob Zebker <jacobzebker@jacobs-air.attlocal.net>
2021-08-27 00:35:55 -0700

    d1 and 2 sorta

--- js/charts.js
+++ js/charts.js
@@ -58,30 +58,54 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-
+    var samplesArray = data.samples;
     // 4. Create a variable that filters the samples for the object with the desired sample number.
-
+    var samplePick = samplesArray.filter(sampleObj => sampleObj.id == sample)
     //  5. Create a variable that holds the first sample in the array.
-
-
+    var firstSample = samplePick[0]
+  
     // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
-
+    var  otu_ids = firstSample.otu_ids
+    var  otu_labels = firstSample.otu_labels
+    var  sample_values = firstSample.sample_values
 
     // 7. Create the yticks for the bar chart.
-    // Hint: Get the the top 10 otu_ids and map them in descending order  
-    //  so the otu_ids with the most bacteria are last. 
-
-    var yticks = 
+    var otu_ids_bar = otu_ids.slice(0,10).map(x=>'OTU '+x.toString())
 
     // 8. Create the trace for the bar chart. 
-    var barData = [
-      
-    ];
+    var barData = [{
+      x: sample_values,
+      y: otu_ids_bar,
+      text: otu_labels,
+      type: "bar", 
+      orientation: 'h'
+    }];
+
     // 9. Create the layout for the bar chart. 
     var barLayout = {
-     
+      title: "Top 10 Bacteria Cultures Found",
+      yaxis: {autorange: "reversed"}
     };
+
     // 10. Use Plotly to plot the data with the layout. 
-    
+    Plotly.newPlot("bar", barData, barLayout)
+
+    // 1. Create the trace for the bubble chart.
+    var bubbleData = [{
+      x: otu_ids,
+      y: sample_values,
+      text: otu_labels,
+      mode: "markers",
+      marker: {size: sample_values}
+    }];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      hovermode: "closest"
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble",bubbleData,bubbleLayout); 
   });
 }
