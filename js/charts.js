da1edb29416b6b59fca2b3c6bfae371adf33f43f
Jacob Zebker <jacobzebker@jacobs-air.attlocal.net>
2021-08-29 18:05:28 -0700

    avg dot

--- js/charts.js
+++ js/charts.js
@@ -59,6 +59,15 @@ function buildCharts(sample) {
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
     var samplesArray = data.samples;
+      // create an array with all of the samples for computing averages
+    allSamples=[]
+    for (let x = 0; x<samplesArray.length;x++) {
+      allSamples.push({
+        otu_ids: samplesArray[x].otu_ids,
+        otu_labels: samplesArray[x].otu_labels,
+        sample_values: samplesArray[x].sample_values
+      })
+    };
     // 4. Create a variable that filters the samples for the object with the desired sample number.
     var samplePick = samplesArray.filter(sampleObj => sampleObj.id == sample)
     //  5. Create a variable that holds the first sample in the array.
@@ -80,6 +89,7 @@ function buildCharts(sample) {
       })
     };
     topTen = topTen.sort((a,b)=>b.sample_values-a.sample_values).slice(0,10)
+      //get the topTen for bar
     var bar_otu_ids = []
     var bar_otu_labels = []
     var bar_sample_values = []
@@ -88,22 +98,55 @@ function buildCharts(sample) {
       bar_otu_labels.push(samp.otu_labels)
       bar_sample_values.push(samp.sample_values)
     }
+      //compute averages per occurrence of each OTU ID
+    var otuTotal = {}
+    var otuCount = {}
+    for (let x=0;x<allSamples.length;x++) {
+      for (let y=0;y<allSamples[x].otu_ids.length;y++){
+        if (allSamples[x].otu_ids[y] in otuTotal){
+          otuTotal[allSamples[x].otu_ids[y]]+=allSamples[x].sample_values[y]
+          otuCount[allSamples[x].otu_ids[y]]+=1
+        }
+        else {
+          otuTotal[allSamples[x].otu_ids[y]]=allSamples[x].sample_values[y]
+          otuCount[allSamples[x].otu_ids[y]]=1
+        }}
+    }
+    var otuAvg = {}
+    for (let x=0;x<Object.values(otuTotal).length;x++){
+      otuAvg['OTU '+Object.keys(otuTotal)[x]] = Object.values(otuTotal)[x]/Object.values(otuCount)[x]
+    }
+      //save values for OTU ID in the bar chart
+    var barAvg=[]
+    for (x of bar_otu_ids) {
+      barAvg.push(otuAvg[x])
+    }
       //trace for bar
-    var barData = [{
+    var barData = {
+      name:"ID: "+sample.toString(),
       x: bar_sample_values,
       y: bar_otu_ids,
       text: bar_otu_labels,
       type: "bar", 
       orientation: 'h'
-    }];
-
+    };
+    var barDataAvg = {
+      name: "Avg",
+      x: barAvg,
+      y: bar_otu_ids,
+      mode: "markers",
+      marker: {color:"black"}
+    }
+    barData = [barData,barDataAvg];
     // 9. Create the layout for the bar chart. 
     var barLayout = {
       title: "Top 10 Bacteria Cultures Found",
       yaxis: {autorange: "reversed"},
       autosize: true,
       plot_bgcolor: '#c7c7c7',
-      hoverlabel: {font:{size:8}}
+      hoverlabel: {font:{size:8}},
+      showlegend:false,
+      hovermode: 'closest'
     };
 
     // 10. Use Plotly to plot the data with the layout. 
@@ -115,7 +158,7 @@ function buildCharts(sample) {
       y: sample_values,
       text: otu_labels,
       mode: "markers",
-      marker: {size: sample_values,color: otu_ids}
+      marker: {size: sample_values.map(x=>.5*x),color: otu_ids}
     }];
 
     // 2. Create the layout for the bubble chart.
